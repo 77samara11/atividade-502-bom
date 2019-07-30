36c7b20e20be9982b8484eee2c3deae6449b4a96
77samara11 <sammycaren73@gmail.com>
2019-07-30 17:58:13 -0300

    atividade 502-bom

--- script.js
+++ script.js
@@ -15,31 +15,37 @@
 let caixaTexto = document.querySelector("input");
 let botao = document.querySelector("button");
 let texto= document.querySelector("p")
-let nãoSou= document.querySelector("a");
+let naoSou = document.querySelector("a");
 let bemvinda= document.querySelector("h1")
 
-}
+
 function  limpar(){
-    localStorage.clear();
+    localStorage.removeItem('nome');
 }
 
-botao.onclick = acessar;
-naoSou.onclick = limpar
 
 if (localStorage.nome){
     caixaTexto.style.display = 'none'
     botao.style.display = 'none'
     bemvinda.innerHTML = `seja bem vinda ${localStorage.nome}`
+    naoSou.innerHTML = `nao e ${localStorage.nome}?`
+
     
 }
 else{
     function acessar(){
         localStorage.setItem("nome", caixaTexto.value);
+        caixaTexto.style.display = 'none'
+        botao.style.display = 'none'
+        bemvinda.innerHTML = `seja bem vinda ${localStorage.nome}`
+        naoSou.innerHTML = `nao e ${localStorage.nome}?`
     }
+    botao.onclick = acessar;
 }
 
 
 
+naoSou.onclick = limpar;
 
 
 
